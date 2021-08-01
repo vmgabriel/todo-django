785830d2fb89a311f2aba9643c6572e095607824
= <vmgabriel96@gmail.com>
2021-08-01 15:32:43 -0500

    feat: change and refresh requirements

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,6 @@ USER_APPLICATIONS = [
 
 EXTERNALS_APPLICATIONS = [
     "versatileimagefield",
-    "social_django",
 ]
 
 
@@ -83,10 +82,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-
-                # Social Django
-                'social_django.context_processors.backends',
-                'social_django.context_processors.login_redirect',
             ],
         },
     },

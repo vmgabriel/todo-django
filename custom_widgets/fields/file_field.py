ce155979b741a53b08a6dba2a4f193b289fe36de
Gabriel <vmgabriel96@gmail.com>
2023-02-19 14:37:09 -0500

    feat: update context of file materialize

--- custom_widgets/fields/file_field.py
+++ custom_widgets/fields/file_field.py
@@ -1,5 +1,10 @@
 from django import forms
 
 
-class MaterializeFileInput(forms.FileInput):
+class MaterializeFileInput(forms.ClearableFileInput):
     template_name = "fields/file.html"
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        print("context - ", context)
+        return context

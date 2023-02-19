77e79a819707e1b6059c5574582f0e2f2172f30b
Gabriel <vmgabriel96@gmail.com>
2023-02-19 14:48:56 -0500

    fix: file upload error django field

--- custom_widgets/fields/file_field.py
+++ custom_widgets/fields/file_field.py
@@ -3,8 +3,3 @@ from django import forms
 
 class MaterializeFileInput(forms.ClearableFileInput):
     template_name = "fields/file.html"
-
-    def get_context(self, name, value, attrs):
-        context = super().get_context(name, value, attrs)
-        print("context - ", context)
-        return context

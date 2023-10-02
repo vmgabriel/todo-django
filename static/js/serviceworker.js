ad4ab586bea800e075183bdbe58bbbaa1a352f9e
Gabriel <vmgabriel96@gmail.com>
2023-10-02 10:55:21 -0500

    feat: service worker context

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -1,20 +1,21 @@
-const staticCacheName = 'djangopwa-v1';
+const staticCacheName = "djangopwa-v1";
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open(staticCacheName).then((cache) => {
             return cache.addAll([
-                '',
+                '/offline',
+                '/css/django-pwa-app.css',
             ]);
         })
     );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
     const requestUrl = new URL(event.request.url);
     if (requestUrl.origin === location.origin) {
-        if ((requestUrl.pathname === '/')) {
-            event.respondWith(caches.match(''));
+        if ((requestUrl.pathname === "/")) {
+            event.respondWith(caches.match(""));
             return;
         }
     }
